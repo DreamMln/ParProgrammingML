5a6761e78750ca9ea67badcf9f2e38063cb2bcd6
DreamMln <maix2374@edu.zealand.dk>
2022-04-06 10:19:10 +0200

    get

--- index.js
+++ index.js
@@ -3,12 +3,32 @@ const baseUrl="https://restduathlon.azurewebsites.net/api/Duathletes"
 const app = Vue.createApp({
     data(){
         return{
-
+            //properties
+            duathletes:[],//array 
+            error:null, //ingen ting ikke nogle objekt
+            postData: {
+                name: "",
+                ageGroup: 0,
+                bike: 0,
+                run: 0 
+            },
+            duathleteMessage: "",
         }
     },
-
  methods:{
-     
+    getAllDuathletes(){
+        this.getAllDuathleteshelper(baseUrl)
+    },
+   async getAllDuathleteshelper(url){
+        try {//fejl håndtering 
+            const result = await axios.get(url)
+            this.duathletes= result.data
+            console.log(this.duathletes)
+            //console.writeline udskriver nogle
+        } catch (ex) {//exception
+            alert(ex.message) 
+        }
+    },
  }
 
 }).mount("#app")
