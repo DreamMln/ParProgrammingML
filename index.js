9406f3b47e02beeb6466ef292ba35ecc4e0b497c
DreamMln <maix2374@edu.zealand.dk>
2022-04-06 10:16:32 +0200

    new

---
+++ index.js
@@ -0,0 +1,14 @@
+const baseUrl="https://restduathlon.azurewebsites.net/api/Duathletes"
+
+const app = Vue.createApp({
+    data(){
+        return{
+
+        }
+    },
+
+ methods:{
+     
+ }
+
+}).mount("#app")
